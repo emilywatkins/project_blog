f1cc97801a6ec00f7be676d6f9a41bf682add316
Emily Watkins <emily.e.dolliver@gmail.com>
2018-04-27 12:53:20 -0700

    fix user/post relationships, add create functionality to posts, show all posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+      flash[:notice] = "post successfully added"
+    else
+      flash[:notice] = "something went wrong, try again"
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
